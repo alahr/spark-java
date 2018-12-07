8d649e9d80ec61fc2b551e04b805654a4dc28ccd
alahr <eyibinghe@sina.com>
2018-12-08 00:31:54 +0800

    add spark dependency and two examples: wordcount and sorted by date

---
+++ src/main/java/com/alahr/spark/example/ProcessDate.java
@@ -0,0 +1,30 @@
+package com.alahr.spark.example;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class ProcessDate {
+    public static void main(String[] args){
+        SparkConf sc = new SparkConf().setAppName("process-date").setMaster("local");
+        JavaSparkContext sparkContext = new JavaSparkContext(sc);
+
+        JavaRDD<String> lines = sparkContext.textFile("shopping.txt");
+
+        JavaPairRDD<String, Integer> ones = lines.mapToPair( line -> {
+            JSONObject obj = JSONObject.parseObject(line);
+            String date = obj.getString("date").substring(0, 16);
+            return new Tuple2<String, Integer>(date, 1);
+        });
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((a, b) -> a+b);
+
+        //true: asc; false: desc
+        counts.sortByKey(true).foreach(t -> System.out.println(t._1+" : "+t._2));
+
+        sparkContext.close();
+    }
+}
---
+++ src/main/java/com/alahr/spark/example/WordCount.java
@@ -0,0 +1,26 @@
+package com.alahr.spark.example;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
+import scala.Tuple2;
+
+import java.util.*;
+
+public class WordCount {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("word-count").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = sc.textFile("english.txt");
+        JavaRDD<String> words = lines.flatMap( line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> ones = words.mapToPair( word -> new Tuple2<>(word, 1));
+        JavaPairRDD<String, Integer> wordCounts = ones.reduceByKey((a,b) -> a+b);
+
+        wordCounts.collect().forEach(t -> System.out.println(t._1+": "+t._2));
+
+        sc.close();
+    }
+}
